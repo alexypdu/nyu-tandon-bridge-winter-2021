1a08bc7d68216bf4c06a1a65003845e1bc28918e
Alex Du <alex.yp.du@gmail.com>
2021-03-13 12:21:47 +0800

    Revise homework 9

--- Week 9/Homework 9/yd2263_hw9_q4.cpp
+++ Week 9/Homework 9/yd2263_hw9_q4.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 void print_array(int arr[], int arr_size);
 void odds_keep_evens_flip(int arr[], int arr_size);
-void swap(int& left, int& right);
-void reverse_array(int arr[], int arr_size);
 
 int main() {
     const int arr_size = 6;
     int arr_1[arr_size] = { 5, 2, 11, 7, 6, 4 };
     int arr_2[arr_size] = { 1, 3, 5, 7, 9, 11 };
-    int arr_3[arr_size] = { 0, 2, 4, 6, 8, 10 };
-    int arr_4[arr_size] = { 9, 13, 20, 8, 5, 7 };
-    int arr_5[arr_size] = { -5, 6, 17, 14, -12, 2 };
+    int arr_3[arr_size] = { 2, 4, 6, 8, 10, 12 };
+    int arr_4[arr_size] = { 1, 3, 5, 2, 4, 6 };
+    int arr_5[arr_size] = { 1, 2, 4, 6, 3, 8 };
 
     cout << "Original array: ";
     print_array(arr_1, arr_size);
@@ -60,28 +58,21 @@ void print_array(int arr[], int arr_size) {
 }
 
 void odds_keep_evens_flip(int arr[], int arr_size) {
-    int odd_count = 0;
+    int left = 0;
+    int right = arr_size - 1;
+    int* temp = new int[arr_size];
 
-    for (int i = 0, j = 0; j < arr_size; j++) {
-        if (arr[j] % 2 != 0) {
-            swap(arr[i++], arr[j]);
-            odd_count++;
+    for (int i = 0; i < arr_size; i++) {
+        if (arr[i] % 2 == 0) {
+            temp[right--] = arr[i];
+        } else {
+            temp[left++] = arr[i];
         }
     }
 
-    reverse_array(arr + odd_count, arr_size - odd_count);
-}
-
-void swap(int& a, int& b) {
-    int temp;
-
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-void reverse_array(int arr[], int arr_size) {
-    for (int i = 0; i < arr_size / 2; i++) {
-        swap(arr[i], arr[arr_size - i - 1]);
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = temp[i];
     }
+
+    delete [] temp;
 }
