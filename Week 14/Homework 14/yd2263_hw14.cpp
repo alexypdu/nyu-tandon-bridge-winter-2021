8d302b264afdbc4a50392cdc42b447b7b56c36e6
Alex Du <alex.yp.du@gmail.com>
2021-04-24 04:13:47 +0800

    Add homework 14

---
+++ Week 14/Homework 14/yd2263_hw14.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> find_min_max(const vector<int>& v, int low, int high);
+vector<int> merge_min_max(const vector<int>& min_max_1, const vector<int>& min_max_2);
+
+int main() {
+    vector<int> v = {9, 4, 8, 7, 3, 0, 6, 5};
+    vector<int> min_max = find_min_max(v, 0, v.size() - 1);
+
+    cout << "min = " << min_max[0] << ", max = " << min_max[1];
+
+    return 0;
+}
+
+vector<int> find_min_max(const vector<int>& v, int low, int high) {
+    if (low == high) {
+        vector<int> min_max(2, v[low]);
+
+        return min_max;
+    } else {
+        int mid = low + (high - low) / 2;
+        vector<int> min_max_1 = find_min_max(v, low, mid);
+        vector<int> min_max_2 = find_min_max(v, mid + 1, high);
+
+        return merge_min_max(min_max_1, min_max_2);
+    }
+}
+
+vector<int> merge_min_max(const vector<int>& min_max_1, const vector<int>& min_max_2) {
+    vector<int> min_max(2);
+    min_max[0] = min(min_max_1[0], min_max_2[0]);
+    min_max[1] = max(min_max_1[1], min_max_2[1]);
+
+    return min_max;
+}
